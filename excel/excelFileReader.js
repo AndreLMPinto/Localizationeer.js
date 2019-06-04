8bb87cdcac2bd4a5f1b30bc1935477e0cc39e868
Pinto <andre.pinto@hp.com>
2019-06-04 09:51:21 -0300

    A string cant have an unnescaped amp.

--- excel/excelFileReader.js
+++ excel/excelFileReader.js
@@ -26,7 +26,8 @@ module.exports = class ExcelFileReader {
         return text.trim()
             .replace(/\'|\u2018|\u2019|\u201b|\u2032/g, "\\\'")
             .replace(/\"|\u201c|\u201d|\u2033/g, "\\\"")
-            .replace(/\r\n/g, "\\n");
+            .replace(/\r\n/g, "\\n")
+            .replace(/&\s/g, "&amp; ");
     }
 
     validate(options) {
